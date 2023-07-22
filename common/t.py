89bf21e04644e249f8752ec4cbf76893f958f53d
SamiraAli <parmer_110@yahoo.com>
2023-07-22 16:44:37 +0330

    m

--- common/t.py
+++
@@ -1,9 +0,0 @@
-from apps import encoder, decoder,sxtw_encoder, sxtw_decoder, aes_encoder, aes_decoder
-
-str = "♦♥"
-encode = encoder(str)
-print("encoded: ", encode)
-print("Lenght: ", len(encode))
-
-decode = decoder(encode)
-print("decoded: ", decode)
