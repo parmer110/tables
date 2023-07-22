a79852739e355b7d472c959d4e091e8856e054bb
SamiraAli <parmer_110@yahoo.com>
2023-07-22 16:01:56 +0330

    C002

--- common/models.py
+++ common/models.py
@@ -5,6 +5,33 @@ class User(AbstractUser):
     id = models.BigAutoField(primary_key=True)
     timestamp = models.DateTimeField(auto_now_add=True, null=True)
 
-class person(models.Model):
+class Person(models.Model):
+    SEX = (
+        ('Male', 'Male'),
+        ('Female', 'Female'),
+    )
     id = models.AutoField(primary_key=True)
-    timestamp = models.DateTimeField(auto_now_add=True)
+    # job1 = models.ForeignKey()
+    # job_n = models.ForeignKey()
+    firstname = models.CharField(max_length=64, null=True)
+    lastname = models.CharField(max_length=80, null=True)
+    birthdate = models.DateField(null=True)
+    national_code = models.CharField(max_length=10, null=True)
+    pasport_number = models.CharField(max_length=8, null=True)
+    sex = models.CharField(max_length=10, choices=SEX, null=True)
+    mobile_phone = models.CharField(max_length=15, null=True)
+    address = models.ForeignKey('Places', null=True, on_delete=models.SET_NULL)
+    createddtm = models.DateTimeField(auto_now_add=True)
+    updatedtm = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField()
+
+class Places(models.Model):
+    title = models.CharField(max_length=128)
+    country = models.CharField(max_length=64, null=True)
+    city = models.CharField(max_length=64, null=True)
+    address = models.CharField(max_length=1024, null=True)
+    usage = models.CharField(max_length=1024, null=True)
+    createddtm = models.DateTimeField(auto_now_add=True)
+    updatedtm = models.DateTimeField(auto_now=True)
+    deleted = models.DateTimeField()
+    
